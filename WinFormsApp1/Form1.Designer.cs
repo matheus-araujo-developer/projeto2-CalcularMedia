9c38807cb0c5521746246480f0b8f1b0bb904bbe
Matheus Araujo de Sousa <matheus2001sousa@hotmail.com>
2024-01-02 20:37:51 -0300

    Commit inicial

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtNota1 = new TextBox();
+            txtNota2 = new TextBox();
+            txtNota3 = new TextBox();
+            btnMedia = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            txtMedia = new TextBox();
+            label5 = new Label();
+            lblSituacao = new Label();
+            label6 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // txtNota1
+            // 
+            txtNota1.Location = new Point(81, 52);
+            txtNota1.Name = "txtNota1";
+            txtNota1.Size = new Size(100, 23);
+            txtNota1.TabIndex = 0;
+            // 
+            // txtNota2
+            // 
+            txtNota2.Location = new Point(81, 89);
+            txtNota2.Name = "txtNota2";
+            txtNota2.Size = new Size(100, 23);
+            txtNota2.TabIndex = 1;
+            // 
+            // txtNota3
+            // 
+            txtNota3.Location = new Point(81, 127);
+            txtNota3.Name = "txtNota3";
+            txtNota3.Size = new Size(100, 23);
+            txtNota3.TabIndex = 2;
+            // 
+            // btnMedia
+            // 
+            btnMedia.Location = new Point(37, 156);
+            btnMedia.Name = "btnMedia";
+            btnMedia.Size = new Size(75, 23);
+            btnMedia.TabIndex = 4;
+            btnMedia.Text = "CALCULAR";
+            btnMedia.UseVisualStyleBackColor = true;
+            btnMedia.Click += btnMedia_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(37, 55);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Nota1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(37, 92);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Nota2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(37, 130);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Nota3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(212, 69);
+            label4.Name = "label4";
+            label4.Size = new Size(46, 15);
+            label4.TabIndex = 8;
+            label4.Text = "MÉDIA:";
+            // 
+            // txtMedia
+            // 
+            txtMedia.Location = new Point(264, 66);
+            txtMedia.Name = "txtMedia";
+            txtMedia.Size = new Size(100, 23);
+            txtMedia.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(212, 97);
+            label5.Name = "label5";
+            label5.Size = new Size(66, 15);
+            label5.TabIndex = 10;
+            label5.Text = "SITUAÇÃO:";
+            // 
+            // lblSituacao
+            // 
+            lblSituacao.AutoSize = true;
+            lblSituacao.Location = new Point(284, 97);
+            lblSituacao.Name = "lblSituacao";
+            lblSituacao.Size = new Size(0, 15);
+            lblSituacao.TabIndex = 11;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(153, 19);
+            label6.Name = "label6";
+            label6.Size = new Size(105, 15);
+            label6.TabIndex = 12;
+            label6.Text = "CALCULAR MÉDIA";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(118, 156);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 13;
+            button1.Text = "LIMPAR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(419, 211);
+            Controls.Add(button1);
+            Controls.Add(label6);
+            Controls.Add(lblSituacao);
+            Controls.Add(label5);
+            Controls.Add(txtMedia);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnMedia);
+            Controls.Add(txtNota3);
+            Controls.Add(txtNota2);
+            Controls.Add(txtNota1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtNota1;
+        private TextBox txtNota2;
+        private TextBox txtNota3;
+        private Button btnMedia;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox txtMedia;
+        private Label label5;
+        private Label lblSituacao;
+        private Label label6;
+        private Button button1;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,41 @@
+using System.Globalization;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        float nota1, nota2, nota3, media;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMedia_Click(object sender, EventArgs e)
+        {
+            nota1 = float.Parse(txtNota1.Text);
+            nota2 = float.Parse(txtNota2.Text);
+            nota3 = float.Parse(txtNota3.Text);
+
+            media = (nota1 + nota2 + nota3) / 3;
+
+            txtMedia.Text = Convert.ToString(media, CultureInfo.InvariantCulture);
+
+            //Condição IF
+            if (media >= 7)
+            {
+                lblSituacao.Text = "APROVADO";
+            }
+            else
+                lblSituacao.Text = "REPROVADO";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtMedia.Text = " ";
+            lblSituacao.Text = " ";
+            txtNota1.Text = " ";
+            txtNota2.Text = " ";
+            txtNota3.Text = " ";
+        }
+    }
+}
